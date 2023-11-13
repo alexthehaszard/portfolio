7309f9f1739ba31b5fb7a64b57331d00bad0406d
Alex Haszard <aggis.haszard@gmail.com>
2023-11-14 11:03:50 +1300

    add yahtzee scorecard & rearrange project cards

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 
 export default function ContactPage() {
   return (
-    <main className={styles.main}>
+    <main className={styles.main} style={{ display: "none" }}>
       <h1>Contact Me</h1>
       <p>Feel free to email me here:</p>
       <a href="mailto:alex.haszard@gmail.com">
