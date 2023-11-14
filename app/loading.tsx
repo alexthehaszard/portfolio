e5e21061e0a01ffbb9630a8d470500bdfdd088f9
Alex Haszard <aggis.haszard@gmail.com>
2023-11-15 02:50:48 +1300

    add loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,8 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <main></main>;
+  return (
+    <main style={{ padding: "30vh 10vw" }}>
+      <h1>Loading, please wait...</h1>
+    </main>
+  );
 }
